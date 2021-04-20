90f09d8fa5a3db2107e0c7821648f8ff1b30cb8d
Frykas <thomas.daenens@hotmail.com>
2021-04-21 01:58:15 +0200

    Katahel ruins dungeon
    Added a new dungeon, the Katahel Ruins (placeDungeon id : starforge-katahelruinsdungeon)
    Added 8 new objects for the dungeon
    Updated sprites for Mariko's weapons (thanks Raiizy)
    Fixed typo in a codex
    Added Decapitator parts item for quest
    Added the Decapitator quest
    Added new treasure pool for dungeon
    Fixed a tileset error

---
+++ objects/scripts/starforgeinteractpickup.lua
@@ -0,0 +1,50 @@
+function init()
+  if not storage.itemHasSpawned then
+    object.setInteractive(true)
+    animator.setAnimationState("objectState", "filled")
+	storage.itemHasSpawned = false
+  else
+    animator.setAnimationState("objectState", "empty")
+	object.setInteractive(false)
+  end
+  
+  self.spawnableItem = config.getParameter("spawnableItem")
+  self.useFloatingObject = config.getParameter("useFloatingObject", false)
+  self.floatingObjectCycle = config.getParameter("floatingObjectCycle", 1.0) / (2 * math.pi)
+  self.floatingObjectMaxTransform = config.getParameter("floatingObjectMaxTransform", 1.0)
+  self.timer = 0
+  
+  --Optionally reset the floating object transformation group
+  if self.useFloatingObject then
+	animator.resetTransformationGroup("floatingObject")
+  end
+end
+
+function update(dt)
+  --Optionally make the artefact float up and down
+  if self.useFloatingObject then
+	self.timer = self.timer + dt
+	local offset = math.sin(self.timer / self.floatingObjectCycle) * self.floatingObjectMaxTransform
+	
+	animator.resetTransformationGroup("floatingObject")
+	animator.translateTransformationGroup("floatingObject", {0, offset})
+  end
+end
+
+function open()
+  animator.setAnimationState("objectState", "empty")
+  world.spawnItem(self.spawnableItem, entity.position(), 1)
+  storage.itemHasSpawned = true
+  object.setInteractive(false)
+  
+  --Make sure that, if the object is broken after having been collected, nothing drops
+  object.setConfigParameter("breakDropPool", "empty")
+end
+
+function onInteraction(args)
+  if storage.itemHasSpawned == false then
+    open()
+  end
+end
+
+--All credits goes to Aegonian for making the script
