b9d663872937c200521cc7b910031c093092f421
Nebulox <42601049+Nebulox@users.noreply.github.com>
2021-10-13 00:54:11 +1000

    Fist Update
    - Adds alt abilities to fists
    - Synergises with vanilla and other modded fists
    - Gives Type-44 Gauntlet a parry altfire, allowing you to block an attack and counter attack with the finisher of the other gauntlet
    - Alts work as such, if the weapon is in the right slot, and the other slot is inhabited by a fist weapon, you can right click to activate the altfire, and left click to execute the combo
    - The downside to this is the non altfire weapon finisher can only be triggered through abnormal means, for example, the altfire itself must trigger it somehow, which is why the parry counter triggers the other fists finisher and not its own
    - Prepares for projectiles in the combo
    - Adds more invincibility time in the punch, making weapon more viable ingame

---
+++ items/active/weapons/fist/starforge-punch.lua
@@ -0,0 +1,91 @@
+require "/scripts/util.lua"
+require "/items/active/weapons/weapon.lua"
+
+-- fist weapon primary attack
+StarforgePunch = WeaponAbility:new()
+
+function StarforgePunch:init()
+  self.damageConfig.baseDamage = self.baseDps * self.fireTime
+
+  self.weapon:setStance(self.stances.idle)
+
+  self.cooldownTimer = self:cooldownTime()
+
+  self.freezesLeft = self.freezeLimit
+  self.freezeTimer = 0
+
+  self.weapon.onLeaveAbility = function()
+    self.weapon:setStance(self.stances.idle)
+  end
+end
+
+-- Ticks on every update regardless if this is the active ability
+function StarforgePunch:update(dt, fireMode, shiftHeld)
+  WeaponAbility.update(self, dt, fireMode, shiftHeld)
+
+  self.cooldownTimer = math.max(0, self.cooldownTimer - self.dt)
+
+  self.freezeTimer = math.max(0, self.freezeTimer - dt)
+  if self.freezeTimer > 0 and not mcontroller.onGround() and math.abs(world.gravity(mcontroller.position())) > 0 then
+    mcontroller.controlApproachVelocity({0, 0}, 1000)
+  end
+end
+
+function StarforgePunch:canStartAttack()
+  return not self.weapon.currentAbility and self.cooldownTimer == 0
+end
+
+-- used by fist weapon combo system
+function StarforgePunch:startAttack()
+  self:setState(self.windup)
+
+  if self.weapon.freezesLeft > 0 then
+    self.weapon.freezesLeft = self.weapon.freezesLeft - 1
+    self.freezeTimer = self.freezeTime or 0
+  end
+end
+
+-- State: windup
+function StarforgePunch:windup()
+  self.weapon:setStance(self.stances.windup)
+
+  util.wait(self.stances.windup.duration)
+
+  self:setState(self.windup2)
+end
+
+-- State: windup2
+function StarforgePunch:windup2()
+  self.weapon:setStance(self.stances.windup2)
+
+  util.wait(self.stances.windup2.duration)
+
+  self:setState(self.fire)
+end
+
+-- State: fire
+function StarforgePunch:fire()
+  self.weapon:setStance(self.stances.fire)
+  self.weapon:updateAim()
+
+  animator.setAnimationState("attack", "fire")
+  animator.playSound("fire")
+
+  status.addEphemeralEffect("invulnerable", self.stances.fire.duration + 0.1)
+
+  util.wait(self.stances.fire.duration, function()
+    local damageArea = partDamageArea("swoosh")
+    
+    self.weapon:setDamage(self.damageConfig, damageArea, self.fireTime)
+  end)
+
+  self.cooldownTimer = self:cooldownTime()
+end
+
+function StarforgePunch:cooldownTime()
+  return self.fireTime - self.stances.windup.duration - self.stances.fire.duration
+end
+
+function StarforgePunch:uninit(unloaded)
+  self.weapon:setDamage()
+end
