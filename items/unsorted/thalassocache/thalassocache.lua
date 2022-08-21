32e106a0bc3f90ab89f4bab54a0bbc3a65c2e372
SilverSokolova <80606782+SilverSokolova@users.noreply.github.com>
2022-08-20 23:14:46 -0500

    optimizations
    remove unused variables, functions, etc

--- items/unsorted/thalassocache/thalassocache.lua
+++ items/unsorted/thalassocache/thalassocache.lua
@@ -1,30 +1,25 @@
-require "/scripts/vec2.lua"
-
 function init()
-  self.recoil = 0
-  self.recoilRate = 0
-
-  self.fireOffset = config.getParameter("fireOffset")
-  updateAim()
-
-  self.active = false
+  recoil = 0
+  recoilRate = 0
+  fireOffset = config.getParameter("fireOffset")
+  active = false
   storage.fireTimer = storage.fireTimer or 0
 end
 
-function update(dt, fireMode, shiftHeld)
+function update(dt)
   updateAim()
 
   storage.fireTimer = math.max(storage.fireTimer - dt, 0)
 
-  if self.active then
-    self.recoilRate = 0
+  if active then
+    recoilRate = 0
   else
-    self.recoilRate = math.max(1, self.recoilRate + (10 * dt))
+    recoilRate = math.max(1, recoilRate + (10 * dt))
   end
-  self.recoil = math.max(self.recoil - dt * self.recoilRate, 0)
+  recoil = math.max(recoil - dt * recoilRate, 0)
 
-  if self.active and not storage.firing and storage.fireTimer <= 0 then
-    self.recoil = math.pi/2 - self.aimAngle
+  if active and not storage.firing and storage.fireTimer <= 0 then
+    recoil = math.pi/2 - aimAngle
     activeItem.setArmAngle(math.pi/2)
     if animator.animationState("firing") == "off" then
       animator.setAnimationState("firing", "fire")
@@ -34,15 +29,12 @@ function update(dt, fireMode, shiftHeld)
 
   end
 
-  self.active = false
+  active = false
 
   if storage.firing and animator.animationState("firing") == "off" then
     item.consume(1)
     if player then
-      local pool = config.getParameter("treasure.pool")
-      local level = config.getParameter("treasure.level")
-      local seed = config.getParameter("treasure.seed")
-      local treasure = root.createTreasure(pool, level, seed)
+      local treasure = root.createTreasure(config.getParameter("treasure.pool"), config.getParameter("treasure.level"), config.getParameter("treasure.seed"))
       for _,item in pairs(treasure) do
         player.giveItem(item)
       end
@@ -52,37 +44,15 @@ function update(dt, fireMode, shiftHeld)
   end
 end
 
-function activate(fireMode, shiftHeld)
+function activate()
   if not storage.firing then
-    self.active = true
+    active = true
   end
 end
 
 function updateAim()
-  self.aimAngle, self.aimDirection = activeItem.aimAngleAndDirection(self.fireOffset[2], activeItem.ownerAimPosition())
-  self.aimAngle = self.aimAngle + self.recoil
-  activeItem.setArmAngle(self.aimAngle)
-  activeItem.setFacingDirection(self.aimDirection)
-end
-
-function firePosition()
-  return vec2.add(mcontroller.position(), activeItem.handPosition(self.fireOffset))
-end
-
-function aimVector()
-  local aimVector = vec2.rotate({1, 0}, self.aimAngle + sb.nrand(config.getParameter("inaccuracy", 0), 0))
-  aimVector[1] = aimVector[1] * self.aimDirection
-  return aimVector
-end
-
-function holdingItem()
-  return true
-end
-
-function recoil()
-  return false
-end
-
-function outsideOfHand()
-  return false
+  aimAngle, aimDirection = activeItem.aimAngleAndDirection(fireOffset, activeItem.ownerAimPosition())
+  aimAngle = aimAngle + recoil
+  activeItem.setArmAngle(aimAngle)
+  activeItem.setFacingDirection(aimDirection)
 end
