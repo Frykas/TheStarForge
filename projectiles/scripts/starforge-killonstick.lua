a4a6ba54478b3d9bd30b7c60bb746ac45826e55b
Nebulox <42601049+Nebulox@users.noreply.github.com>
2022-11-05 22:18:16 +1000

    Minor Updates
    - Adds the Fury shotgun
    - Updates some file paths
    - Adds wither thorns

--- projectiles/scripts/starforge-killonstick.lua
+++ projectiles/scripts/starforge-killonstick.lua
@@ -4,7 +4,7 @@ local baseHit = hit
 function update(dt)
   if baseUpdate then baseUpdate(dt) end
   
-  if projectile.collision() or mcontroller.isCollisionStuck() or mcontroller.isColliding() then
+  if (not config.getParameter("canBounce", false)) and (projectile.collision() or mcontroller.isCollisionStuck() or mcontroller.isColliding()) then
 	projectile.die()
   end
 end
