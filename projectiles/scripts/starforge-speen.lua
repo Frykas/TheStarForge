891467a9ea427a7b86799176914224deca9d5ded
Nebulox <42601049+Nebulox@users.noreply.github.com>
2022-11-16 16:34:07 +1000

    Minor Updates
    - Completely reformats how swooshes are organised
    - Has folders dedicated to swoosh types, with suffixes based on the variant of that swoosh
    - Adds some more swoosh types for later
    - Cleans up a few other things
    - Removes some file bloat

--- projectiles/scripts/starforge-speen.lua
+++ projectiles/scripts/starforge-speen.lua
@@ -1,7 +1,7 @@
 require "/scripts/util.lua"
 
 function init()
-  self.spinDirection = config.getParameter("spinDirection", -1)
+  self.spinDirection = config.getParameter("spinDirection", 1)
   self.spinRate = config.getParameter("spinRate", 0.5)
 end
 
