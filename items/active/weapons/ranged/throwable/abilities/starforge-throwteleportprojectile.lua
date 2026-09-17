97c7d8dda2ac0b4351de91c9fa821107f229808a
Nebulox <42601049+Nebulox@users.noreply.github.com>
2026-09-18 01:03:47 +1000

    Minor Updates
    - Gives Stormcrest's warp strikes a custom sound
    - Fixes a bug where rarely the teleport chakram will not record the cooldown
    - Adjusts the Smart Shotgun's sonar bandwidth to avoid lag

--- items/active/weapons/ranged/throwable/abilities/starforge-throwteleportprojectile.lua
+++ items/active/weapons/ranged/throwable/abilities/starforge-throwteleportprojectile.lua
@@ -175,7 +175,7 @@ function StarforgeThrowTeleportProjectile:cooldown()
   self.weapon.aimAngle = 0
   
   while world.entityExists(config.getParameter("weaponThrown")) do
-    self.teleportTimer = self.teleportTimer + self.dt
+    self.teleportTimer = (self.teleportTimer or 0) + self.dt
 
     if (not self.weapon.currentAbility
       and self.fireMode == (self.activatingFireMode or self.abilitySlot))
